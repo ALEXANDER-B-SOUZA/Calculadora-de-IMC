0571c9d5ed2c749d2afa211fb75d1c3ce4bb91b8
Alexander Batista de Souza <119431061+ALEXANDER-B-SOUZA@users.noreply.github.com>
2023-01-11 20:33:40 -0300

    Update main.js
    Pegando variável do usuário.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+/*Chamar função ao usuário digitar algum valor no campo "receber_nome"*/
+
+function receber_nome(){
+    var nome = document.getElementById("nome").value;
+    console.log(nome);
+
+}
+
 /* função para calcular IMC*/
 
     let altura = 1.82;
