14aecd3a6184dcffbc49f45e6f828896aa63a9ed
Alexander Batista de Souza <119431061+ALEXANDER-B-SOUZA@users.noreply.github.com>
2023-01-12 20:43:14 -0300

    Pegando dados

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-/*Chamar função ao usuário digitar algum valor no campo "receber_nome"*/
+/*Chamar função ao usuário digitar algum valor no campo */
+function calcular(){
+    let nome =  document.getElementById("nome").value;
+    let idade =  document.getElementById("idade").value;
+    let altura =  document.getElementById("altura").value;
+    let peso =  document.getElementById("peso").value;
+    let sexo =  document.getElementById("sexo").value;
+/* função para calcular IMC */
 
-function receber_nome(){
-    var nome = document.getElementById("nome").value;
-    console.log(nome);
-
-}
-
-/* função para calcular IMC*/
-
-    let altura = 1.82;
-    let peso = 110;  
 let resultado =  peso / (altura * altura);
 console.log(resultado);
+}
