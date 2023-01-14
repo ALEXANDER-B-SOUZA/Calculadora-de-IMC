491b523f296ce5b3302cede29ef19aa92f620c98
Alexander Batista de Souza <119431061+ALEXANDER-B-SOUZA@users.noreply.github.com>
2023-01-14 16:52:13 -0300

    Regras para resposta

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 /*Chamar função ao usuário digitar algum valor no campo */
 function calcular(){
-    let nome =  document.getElementById("nome").value;
-    let idade =  document.getElementById("idade").value;
+    let nome =  document.getElementById("nome").value;    
     let altura =  document.getElementById("altura").value;
-    let peso =  document.getElementById("peso").value;
-    let sexo =  document.getElementById("sexo").value;
+    let peso =  document.getElementById("peso").value;   
 /* função para calcular IMC */
 let alturacm = altura/100;
-let IMC =  peso / (alturacm * alturacm);
-/* Enviando resultado*/
-document.getElementById("resultado").innerHTML = nome+ " seu IMC é: "+IMC 
-console.log(idade);
-console.log(sexo);
+let IMC =  Math.round( peso / (alturacm * alturacm));
+/*comparação para resultado*/
+if(IMC <= 18,5){
+ document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está abaixo do peso.";
+ } else if((IMC > 18,5) || (IMC <= 24,9)){
+document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com o peso normal.";
+ } else if((IMC > 25) || (IMC <= 29,9)){
+document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com excesso de peso.";
+ } else if ((IMC > 30) || (IMC <= 34,9)){
+document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com obesidade grau I";
+ } else if((IMC > 35) || (IMC<= 39,9)){
+document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com obesidade grau II";
+ } else if (IMC >= 40){
+document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com obesidade grau III";
+ }
 }
