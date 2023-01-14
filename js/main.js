7b41816b8a4cef3290ac9962c9db2714d2be20ff
Alexander Batista de Souza <119431061+ALEXANDER-B-SOUZA@users.noreply.github.com>
2023-01-14 17:03:08 -0300

    Melhorias nas regras

--- js/main.js
+++ js/main.js
@@ -7,17 +7,18 @@ function calcular(){
 let alturacm = altura/100;
 let IMC =  Math.round( peso / (alturacm * alturacm));
 /*comparação para resultado*/
-if(IMC <= 18,5){
- document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está abaixo do peso.";
- } else if((IMC > 18,5) || (IMC <= 24,9)){
-document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com o peso normal.";
- } else if((IMC > 25) || (IMC <= 29,9)){
-document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com excesso de peso.";
- } else if ((IMC > 30) || (IMC <= 34,9)){
-document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com obesidade grau I";
- } else if((IMC > 35) || (IMC<= 39,9)){
-document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com obesidade grau II";
+if(IMC <= 18.5){
+ document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está abaixo do peso."
+ } else if((IMC > 18.5) && (IMC <= 24.9)){
+document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com o peso normal."
+ } else if((IMC > 25) && (IMC <= 29.9)){
+document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com excesso de peso."
+ } else if ((IMC > 30) && (IMC <= 34.9)){
+document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com obesidade grau I"
+ } else if((IMC > 35) && (IMC<= 39.9)){
+document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com obesidade grau II"
  } else if (IMC >= 40){
-document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com obesidade grau III";
+document.getElementById("resultado").innerHTML = nome+ ", seu IMC é: "+IMC +". Você está com obesidade grau III"
  }
 }
+
